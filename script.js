fdcbef880b73c6ff08a2dd4039d3c02f67537e4a
Busola Tom <busolatom@gmail.com>
2024-07-23 19:32:51 +0000

    random number generator

---
+++ script.js
@@ -0,0 +1,11 @@
+const number = document.getElementById('number');
+const generate = document.getElementById('generate');
+
+const randomNumberGenerator = () => {
+    const randomNumber = Math.floor(Math.random() * 100000000 + 1);
+    number.textContent = randomNumber;
+};
+
+generate.addEventListener('click', randomNumberGenerator);
+
+randomNumberGenerator();
